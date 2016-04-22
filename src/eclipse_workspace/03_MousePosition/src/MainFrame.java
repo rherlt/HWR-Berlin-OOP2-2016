64b745233d6e44059f12e771f5d30bc2f0d6dba0
Rico Herlt <rherlt@h3ko.de>
2016-04-22 14:41:06 +0200

    added click count events

--- src/eclipse_workspace/03_MousePosition/src/MainFrame.java
+++ src/eclipse_workspace/03_MousePosition/src/MainFrame.java
@@ -1,7 +1,9 @@
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /***
  * Main window of the application. Please notice that this window is implementing
@@ -10,10 +12,11 @@ import javax.swing.JFrame;
  *
  *
  */
-public class MainFrame extends JFrame implements MouseMotionListener{
+public class MainFrame extends JFrame implements MouseMotionListener, MouseListener{
     
     private static final long serialVersionUID = -979691148534081065L;
-    
+    private JLabel label;
+    private int clickCount = 0;
        /**
      * constructor of the window
      * @param title which is displayed as window title
@@ -24,7 +27,11 @@ public class MainFrame extends JFrame implements MouseMotionListener{
         //add a listener for mouse motions. Use this class as
         //listener which is called by the framework when a mouse
         //motion is detected
-        this.addMouseMotionListener(this);        
+        this.addMouseMotionListener(this); 
+        this.addMouseListener(this);
+        
+        label = new JLabel("Clicks: 0");
+		this.getContentPane().add(label);
     }
 
     /**
@@ -57,6 +64,58 @@ public class MainFrame extends JFrame implements MouseMotionListener{
 		//create a text containing the positions and write them to the current frame title
 		String position = String.format("X: %.2f - Y: %.2f ", xCoord, yCoord);
 		this.setTitle(position);
+	}	
+	
+	/**
+     * Override the mouseClicked method of the interface MouseListener.
+     * This method is called by the framework when the mouse is clicked
+     */
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+		clickCount++;
+		String text = String.format("Clicks: %d", clickCount);
+		label.setText(text);
+	}
+
+	/**
+     * Override the mousePressed method of the interface MouseListener.
+     * This method is called by the framework when the mouse button is pressed
+     */
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+     * Override the mouseReleased method of the interface MouseListener.
+     * This method is called by the framework when the mouse button is released
+     */
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+     * Override the mouseEntered method of the interface MouseListener.
+     * This method is called by the framework when the mouse entered this component
+     */
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+     * Override the mouseExited method of the interface MouseListener.
+     * This method is called by the framework when the mouse exited this component
+     */
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
   
 }
